7177d9e986ada451c75647a5467717ec4e2e30b4
Lenson Merwin Fernandes <lenson.fernand@gmail.com>
2023-02-11 11:38:16 +0530

    Added useEffect to Handle async of setState

--- calculator/src/components/Input.jsx
+++ calculator/src/components/Input.jsx
@@ -1,13 +1,63 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
 const Input = () => {
-  return (
-    <div className='d-flex flex-column gap-2'>
-        <input type="text" placeholder="Num 1"  className='bg-black text-white border-white'/> 
-        <input type="text" placeholder="Num 2" className='bg-black text-white border-white'/> 
-        
-    </div>
-  )
-}
+  let [num1, setNum1] = useState("");
+  let [num2, setNum2] = useState("");
 
-export default Input
+  let runCalculate = () =>{
+
+    checkValidation();
+    findResult();
+ }
+
+ let checkValidation = () =>{
+
+ }
+
+ let findResult = () =>{
+
+ }
+ useEffect(()=>{
+    console.log(num1)
+ }, [num1])
+
+ useEffect(()=>{
+    console.log(num2)
+ }, [num2])
+
+  return (
+    <div className="d-flex flex-column gap-2">
+      <input
+        type="text"
+        placeholder="Num 1"
+        className="bg-black text-white border-white"        
+        value={num1}
+        onChange={(e) => {
+             setNum1(e.target.value);
+          //  console.log(num1);
+          }}
+        
+      />
+      <input
+        type="text"
+        placeholder="Num 2"
+        className="bg-black text-white border-white"
+        value={num2}
+        onChange={(e) => {
+          setNum2(e.target.value);
+      
+        }}
+        
+      />
+
+<div className='row ' >
+        <button className='col-auto ms-2' onClick={runCalculate}> + </button>
+        <button className='col-auto'> - </button>
+        <button className='col-auto'> * </button>
+        <button className='col-auto'> / </button>
+    </div>
+    </div>
+  );
+};
+
+export default Input;
--- calculator/src/components/Operators.jsx
+++ calculator/src/components/Operators.jsx
@@ -1,14 +1,29 @@
 import React from 'react'
 
 const Operators = () => {
-  return (
-    <div className='row ' >
-        <button className='col-auto ms-2'> + </button>
-        <button className='col-auto'> - </button>
-        <button className='col-auto'> * </button>
-        <button className='col-auto'> / </button>
-    </div>
-  )
+
+//  let runCalculate = () =>{
+
+//     checkValidation();
+//     findResult();
+//  }
+
+//  let checkValidation = () =>{
+
+//  }
+
+//  let findResult = () =>{
+
+//  }
+
+//   return (
+//     // <div className='row ' >
+//     //     <button className='col-auto ms-2' onClick={runCalculate}> + </button>
+//     //     <button className='col-auto'> - </button>
+//     //     <button className='col-auto'> * </button>
+//     //     <button className='col-auto'> / </button>
+//     // </div>
+//   )
 }
 
 export default Operators
